28c39661f3676f7b3fa9937f1edb02d587c194c7
CC0454 <22cmcpike@wakatipu.school.nz>
2024-05-28 14:31:54 +1200

    changed the main table's name to student table
    This was done in preparation for a second table for the teachers

--- main_prodject.py
+++ main_prodject.py
@@ -4,9 +4,10 @@ conn = sqlite3.connect('highschool_prodject.db')
 #This line connects to the database.
 cursor = conn.cursor()
 #This makes the little flashing writey thing, I dont know much about the top three lines.
-cursor.execute('SELECT * FROM main_table')
+cursor.execute('SELECT * FROM student_table')
 #This line uses a SQlite sequence and takes the data and puts it in a list.
 main_list = cursor.fetchall()
 #This line gets the list and assigns it to a varible.
 print(main_list)
 #This line prints the varible.
+
