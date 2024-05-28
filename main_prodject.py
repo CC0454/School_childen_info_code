46fcd929e9daf813a46dbc490aadb200106194db
CC0454 <22cmcpike@wakatipu.school.nz>
2024-05-28 13:01:14 +1200

    This is version one of the code, printing all the information at once.
    This version includes a connection to my SQLite database.

--- main_prodject.py
+++ main_prodject.py
@@ -1,1 +1,12 @@
-print("is this updating?")
+import sqlite3
+#This line imports the sqlite database.
+conn = sqlite3.connect('highschool_prodject.db')
+#This line connects to the database.
+cursor = conn.cursor()
+#This makes the little flashing writey thing, I dont know much about the top three lines.
+cursor.execute('SELECT * FROM main_table')
+#This line uses a SQlite sequence and takes the data and puts it in a list.
+main_list = cursor.fetchall()
+#This line gets the list and assigns it to a varible.
+print(main_list)
+#This line prints the varible.
